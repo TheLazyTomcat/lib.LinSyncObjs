43efac31a5034a13eb8029f411e3259433934601
František Milt <frantisek.milt@gmail.com>
2022-02-13 22:52:22 +0100

    Initial commit, 2022-01-13 22:52 CET

---
+++ LinSyncObjs.pas
@@ -0,0 +1,19 @@
+unit LinSyncObjs;
+
+{$IF Defined(LINUX) and Defined(FPC)}
+  {$DEFINE Linux}
+{$ELSE}
+  {$MESSAGE FATAL 'Unsupported operating system.'}
+{$IFEND}
+
+{$IFDEF FPC}
+  {$MODE ObjFPC}
+{$ENDIF}
+{$H+}
+
+interface
+
+implementation
+
+end.
+
