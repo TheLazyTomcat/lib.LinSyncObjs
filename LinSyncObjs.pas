543ec38b5d82940dd4f9fe5e24cacf0aaa81aaca
František Milt <frantisek.milt@gmail.com>
2022-08-13 13:56:43 +0200

    Multi-wait functions can now return error code indication a cause of failure

--- LinSyncObjs.pas
+++ LinSyncObjs.pas
@@ -30,7 +30,7 @@
              problems. Use them with caution and if you find any bugs, please
              report them.
 
-  Version 1.0.1 (2022-08-13)
+  Version 1.0.2 (2022-08-13)
 
   Last change 2022-08-13
 
@@ -725,12 +725,7 @@ type
 --------------------------------------------------------------------------------
 ===============================================================================}
 {
-  Value of output variable Index is undefined in most cases. Only when WaitAll
-  is set to false and the function returns with result being wrSignaled the
-  index will contain zero-based index of object that caused the function to
-  return.
-
-  Given the implementation, there are limitations in effect for the following
+  Given the implementation, there are limitations in effect for multi-wait
   functions, those are:
 
     - there is a system-wide limit of 15360 concurently running instances of
@@ -742,7 +737,26 @@ type
     - the number of events in parameter Objects is not strictly limited, but it
       is not recommended to go above 32 (there is a technical limit of about 4
       bilion objects)
+
+
+  When WaitAll is set to false and the function returns wrSignaled, then the
+  output parameter Index will contain zero-based index of object that caused
+  the function to return.
+
+  When wrError is returned, then the index is set to one of the following error
+  codes to indicate what caused the function to fail.
+
+  In all other cases, the value of Index is undefined.
 }
+const
+  LSO_WAITERROR_UNKNOWN   = 1;  // some unknown or unspecified error
+  LSO_WAITERROR_COUNT     = 2;  // zero object count
+  LSO_WAITERROR_OBJECTS   = 3;  // invalid object(s) (unassigned, duplicate, ...)
+  LSO_WAITERROR_NOSLOT    = 4;  // reached limit of concurently running instances of multi-wait
+  LSO_WAITERROR_EVENTFULL = 5;  // at least one event has full waiter array and cannot be waited upon
+  LSO_WAITERROR_FUTEXWAIT = 6;  // error in internla waiting mechanism
+
+//------------------------------------------------------------------------------
 
 Function WaitForMultipleEvents(Objects: array of PLSOEvent; WaitAll: Boolean; Timeout: DWORD; out Index: Integer): TLSOWaitResult;
 Function WaitForMultipleEvents(Objects: array of PLSOEvent; WaitAll: Boolean; Timeout: DWORD): TLSOWaitResult;
@@ -3829,6 +3843,14 @@ end;
 
 Function WaitForMultipleEvents_Internal(Objects: array of PLSOEvent; Timeout: DWORD; WaitAll: Boolean; out Index: Integer): TLSOWaitResult;
 
+  procedure ReturnError(ErrorCode: Integer);
+  begin
+    Index := ErrorCode;
+    Result := wrError;
+  end;
+
+//   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---   ---
+
   Function CheckObjects: Boolean;
   var
     i,j:  Integer;
@@ -4000,7 +4022,7 @@ try
                   fwrTimeout:     Result := wrTimeout;  // exit with timeout
                   fwrInterrupted: ExitWait := False;    // recalculate timeout and re-enter waiting
                 else
-                  Result := wrError;
+                  ReturnError(LSO_WAITERROR_FUTEXWAIT);
                 end;
                 // recalculate timeout if not exiting
                 If not ExitWait then
@@ -4013,16 +4035,17 @@ try
             finally
               // remove waiter futex from all events
               RemoveSelfFromWaiters;
-            end;
+            end
+          else ReturnError(LSO_WAITERROR_EVENTFULL);
         finally
           // return waiter futex
           MultiWaitSlots.InvalidateSlot(WaiterFutexIdx);
         end
-      else Result := wrError;
+      else ReturnError(LSO_WAITERROR_NOSLOT);
     end
-  else Result := wrError;
+  else ReturnError(LSO_WAITERROR_OBJECTS);
 except
-  Result := wrError;
+  ReturnError(LSO_WAITERROR_UNKNOWN);
 end;
 end;
 
@@ -4031,6 +4054,13 @@ end;
 ===============================================================================}
 
 Function WaitForMultipleEvents(Objects: array of PLSOEvent; WaitAll: Boolean; Timeout: DWORD; out Index: Integer): TLSOWaitResult;
+
+  procedure ReturnError(ErrorCode: Integer);
+  begin
+    Index := ErrorCode;
+    Result := wrError;
+  end;
+
 begin
 Index := -1;
 try
@@ -4044,13 +4074,13 @@ try
           If ThrErrorCode = ESysETIMEDOUT then
             Result := wrTimeout
           else
-            Result := wrError;
+            ReturnError(LSO_WAITERROR_FUTEXWAIT);
         end
       else Result := wrSignaled;
     end
-  else Result := wrError;
+  else ReturnError(LSO_WAITERROR_COUNT);
 except
-  Result := wrError;
+  ReturnError(LSO_WAITERROR_UNKNOWN);
 end;
 end;
 
